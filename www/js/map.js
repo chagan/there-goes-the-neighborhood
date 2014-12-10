fa61e792fc9483798e0a668f705a7aea4b09bdef
chagan <chrishagan23@gmail.com>
2014-12-10 14:34:30 -0600

    Add base map

--- www/js/map.js
+++ www/js/map.js
@@ -1,4 +1,38 @@
-    var map = L.map('map')
-      .setView([41.8947, -87.6105],10)
+    var map = L.map('map',{
+    	zoomControl:false,
+    });
 
-    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png').addTo(map)
+    function getColor(index){
+    	console.log(index);
+    	if (index > 0) {
+    		return "green";
+    	} else {
+    		return "red";
+    	}
+    }
+
+    $.getJSON("assets/gentrification_map.geojson", function(data) {
+    	var geojson = L.geoJson(data, {
+		    style: function (feature) {
+		        return {
+			        weight: 2,
+			        opacity: 1,
+			        color: 'white',
+			        fillColor:getColor(+feature.properties["CA Data for WBEZ_S_2010"]),
+			        fillOpacity: 0.7
+			    };
+		    },
+		    onEachFeature: function (feature, layer) {
+		        layer.bindPopup(feature.properties["CA Data for WBEZ_Community_Area"]);
+		    }
+		}).addTo(map);
+		map.fitBounds(geojson.getBounds());
+		// Disable drag and zoom handlers.
+		map.dragging.disable();
+		map.touchZoom.disable();
+		map.doubleClickZoom.disable();
+		map.scrollWheelZoom.disable();
+
+		// Disable tap handler, if present.
+		if (map.tap) map.tap.disable();
+	});
